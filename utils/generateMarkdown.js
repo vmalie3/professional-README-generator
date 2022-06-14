f3d76819ab50a34e6ae012d49804a14c40b35651
Victoria Malie <vmalie3@gmail.com>
2022-06-14 17:08:16 -0500

    began writing license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,33 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === '') '';
+  else {
+    return'![license](https://img.shields.io/badge/license-${license})';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === '') '';
+  else {
+    return 'This project is licensed by ${license}';
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return 
+`# ${data.title}
 
 `;
 }
