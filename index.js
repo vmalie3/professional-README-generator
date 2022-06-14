a95b44b13226d70d1048e32a607995536e13e710
Victoria Malie <vmalie3@gmail.com>
2022-06-14 15:02:08 -0500

    added questions

--- index.js
+++ index.js
@@ -2,10 +2,59 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the project\'s title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the project\'s description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How do you install/run the project?',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'How do you use the project?',
+        name: 'usage',
+    },
+    {
+        type: 'rawlist',
+        message: 'What License was this created with?',
+        name: 'license',
+        choices: [
+            'NPL',
+            'GPL',
+            'Apache',
+            'MIT',
+            'CC',
+            'BSD'
+        ],
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to the project?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'How can you test the project?',
+        name: 'test',
+    },
+
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
